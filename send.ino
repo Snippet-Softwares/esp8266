19ec61abe74473c3361e25fafeb92d1dfe52bbd6
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2021-10-18 19:13:44 +0300

    Finished setting up payload after result 200

--- send.ino
+++ send.ino
@@ -5,29 +5,36 @@
 String str;
 
 void setup() {
-  Serial.begin(115200);                                 //Serial connection
-  WiFi.begin("CMT_M-1", "EMuema@123");                  //WiFi connection
-  while (WiFi.status() != WL_CONNECTED) {               //Wait for the WiFI connection completion
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);                                         //Serial connection
+  WiFi.begin("CMT_M-1", "EMuema@123");                          //WiFi connection
+  while (WiFi.status() != WL_CONNECTED) {                       //Wait for the WiFI connection completion
     delay(500);
     Serial.println("Waiting for connection");
   }
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {                  //Check WiFi connection status
-    HTTPClient http;                                    //Declare object of class HTTPClient
+  if (WiFi.status() == WL_CONNECTED) {                          //Check WiFi connection status
+    HTTPClient http;                                            //Declare object of class HTTPClient
     WiFiClient client;
-    http.begin(client, "http://snippet.io.ke/tal/");    //Specify request destination
-    http.addHeader("Content-Type", "text/plain");       //Specify content-type header
+    http.begin(client, "http://snippet.io.ke/tal/");            //Specify request destination
+    http.addHeader("Content-Type", "text/plain");               //Specify content-type header
     int value = random(1000);
-    int httpCode = http.POST(String(value));            //Send the request
-    String payload = http.getString();                  //Get the response payload
-    Serial.write(httpCode);                             //Print HTTP return code
-    Serial.println(payload);                            //Print request response payload
-    http.end();                                         //Close connection
+    int httpCode = http.POST(String(value));                    //Send the request
+    String payload = http.getString();                          //Get the response payload
+    Serial.println(httpCode);                                   //Print HTTP return code
+    Serial.println(payload);                                    //Print request response payload
+    http.end();                                                 //Close connection
+    if(payload == "success") {
+      digitalWrite(2, HIGH);
+    }
+    else {
+      digitalWrite(2, LOW);
+    }
   }
   else {
     Serial.println("Error in WiFi connection");
   }
-  delay(5000);                                          //Send a request every x(1000) seconds
+  delay(5000);                                                  //Send a request every x(1000) seconds
 }
